43d6b02f60c191c266cae3f30f3bdb348785d1ca
Sasha <sashabayan@gmail.com>
2021-08-25 09:53:42 -0700

    feat: create utils for cards, suits, and decks
    When writing #drawFromDeck, I felt a bit torn on whether to use mutating vs non-mutating approach to the deck. I chose the mutating approach given the scope of the exercise and that the use case anticipates only one deck used at a time. This approach uses the semantically familiar JS array util .splice, which I felt was appropriate given its simplicity. I will consider refactoring to a non-mutating approach later in the process.
    
    I’ll also note that this util files contains business logic (card generation + deck mutation) and visual representation logic (#valueToDisplay). Similar to points noted above, I figured that the coupling of these types of logic is appropriate given the small scope of the exercise. In a larger project, I would likely separate these forms of logic into separate utility files.

--- frontend/src/utilities.ts
+++ frontend/src/utilities.ts
@@ -9,3 +9,46 @@ export interface Card {
   value: number;
   suit: Suit;
 }
+
+export function createDeck(): Card[] {
+  return [
+    ...createSuit(Suit.Clubs),
+    ...createSuit(Suit.Diamonds),
+    ...createSuit(Suit.Hearts),
+    ...createSuit(Suit.Spades),
+  ];
+}
+
+export function createSuit(suit: Suit): Card[] {
+  const cards = [];
+  for (let i = 1; i < 14; i++) {
+    cards.push({
+      value: i,
+      suit,
+    });
+  }
+  return cards;
+}
+
+export function drawFromDeck(deck: Card[]): Card {
+  // NOTE: this function mutates the original deck
+  // Considering rewriting to keep deck immutable by using slice
+  // But given the nature of the exercise, may be simpler to keep it mutuable
+  const randomIndex = Math.floor(Math.random() * deck.length);
+  return deck.splice(randomIndex, 1)[0];
+}
+
+export function valueToDisplay(val: number): string {
+  switch (val) {
+    case 1:
+      return 'A';
+    case 11:
+      return 'J';
+    case 12:
+      return 'Q';
+    case 13:
+      return 'K';
+    default:
+      return val.toString();
+  }
+}
