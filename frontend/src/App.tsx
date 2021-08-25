5754b92a06f867369e4c3ba4f0367d100a1ba62f
Sasha <sashabayan@gmail.com>
2021-08-25 09:32:07 -0700

    chore: add local Prettier config
    This commit adds a .prettierrc file so that any other developers using a different environment can use the same formatting rules. Adding an empty object ({}) in the config file sets the project to use Prettier’s default rules. The file changes here mostly represent the shifting from double quotes to single quotes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './App.module.css';
-import logo from './logo.svg';
+import React from "react";
+import styles from "./App.module.css";
+import logo from "./logo.svg";
 
 const App: React.FC = () => {
   return (
