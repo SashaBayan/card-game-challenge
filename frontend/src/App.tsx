d4ae8cbc13f5628dca5841bf219f802c6c38b8de
Sasha <sashabayan@gmail.com>
2021-08-25 12:09:12 -0700

    feat: add gradient to bg to imitate card table
    Here I am tailwind CSS’s basic green colors rather than matching the exact design on the spec. My understanding is that the demands on this project are not to match the figma spec precisely but approximately, with prioritization on semantics usage of tailwind CSS classes. I recognize that adding custom CSS colors to tailwind is an option here, but I am making the assumption that this is not the priority. Calling that out here to note that this choice was taken into consideration.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,7 @@ import styles from './App.module.css';
 import logo from './logo.svg';
 
 const App: React.FC = () => {
-  return (
-    <div className="bg-gray-200 p-10 min-h-full">
-      <div className={styles.body}>
-        <img className="h-16 w-16 mx-auto" src={logo} alt="React" />
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    </div>
-  );
+  return <div className={styles.body}></div>;
 };
 
 export default App;
